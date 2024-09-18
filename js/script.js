6cf7d315d9e253d257a2b79895ac2f25394c684f
Joshua Langeveldt <langeveldt20joshua@gmail.com>
2024-09-18 16:09:48 +0200

    Updated Files

--- js/script.js
+++ js/script.js
@@ -1,83 +1,43 @@
-
-
+// Menu Toggle Functionality
 let menuToggle = document.querySelector('.menuToggle');
 let header = document.querySelector('header');
 let section = document.querySelector('section');
-// let section = document.getElementsByTagName('*');
-
 
 menuToggle.onclick = function () {
-    header.classList.toggle('active')
-    section.classList.toggle('active')
-}
+    header.classList.toggle('active');
+    section.classList.toggle('active');
+};
 
+// Slide Functionality
 const slides = document.querySelectorAll('.slide');
-
-// console.log(slides)
-
 let counter = 0;
-// console.log(i)
-
-
 
 slides.forEach((slide, index) => {
-    slide.style.left = `${index * 100}%`
-
+    slide.style.left = `${index * 100}%`;
 });
 
 const goPrev = () => {
-    counter--
-    slideImg()
-}
-const goNext = () => {
-    counter++ 
-    slideImg()
-}
+    counter--;
+    slideImg();
+};
 
+const goNext = () => {
+    counter++;
+    slideImg();
+};
 
 const slideImg = () => {
     for (let i = 0; i < slides.length; i++) {
         if (counter >= slides.length) {
             slides.forEach(slide => {
-                slide.style.transform = `translateX(-${counter * 0}%)`
+                slide.style.transform = `translateX(-${counter * 0}%)`;
             });
         } else {
             slides.forEach(slide => {
-                slide.style.transform = `translateX(-${counter * 100}%)`
+                slide.style.transform = `translateX(-${counter * 100}%)`;
             });
         }
     }
-}
-// const slideImg = () => {
-//     for (let i = 0; i < slides.length; i++) {
-//         if (counter >= slides.length) {
-//             slides.forEach(slide => {
-//                 slide.style.transform = `translateX(-${counter * 0}%)`
-//             });
-//         } else {
-//             slides.forEach(slide => {
-//                 slide.style.transform = `translateX(-${counter * 100}%)`
-//             });
-//         }
-//     }
-// }
+};
 
-
-// setInterval(() => {
-    // if (counter < 2) {
-    //     console.log(counter++)
-    //     slides.forEach((slide, index) => {
-    //         slide.style.left = `${index * 100}%`
-    //         slide.style.transform = `translateX(-${counter * 100}%)`
-    //     });
-    // } 
-    
-    // else {
-    //     console.log(counter--)
-    //     slides.forEach((slide, index) => {
-    //         slide.style.left = `${index * 100}%`
-    //         slide.style.transform = `translateX(-${counter * 100}%)`
-    //     }); 
-    // }
-// }, 2000);
 
